3b6170d1927ca6c8cb337923894f458e01e9d998
양지훈 <kelvin9149@gmail.com>
2021-10-13 02:46:28 +0900

    211013_update

--- SWEA/D2/1204.py
+++ SWEA/D2/1204.py
@@ -1,19 +1,13 @@
-def most_num():
-    T = int(input())
-    num_test = []
-    num_list = []
-    for i in range(T):
-        num_test.append(int(input()))
-        num_list.append(list(map(int, input().split(' '))))
-    
-    result = []
-    for n in num_list:
-        temp = [0, ]*100
-        for j in n:
-            temp[j-1] += 1
-        result.append(temp.index(max(temp[::-1]))+1)
-    
-    for c in range(len(result)):
-        print(f'#{num_test[c]} {result[c]}')
-
-most_num()
+for _ in range(int(input())):
+    tc = int(input())
+    nums = list(map(int, input().split()))
+    ans = [0]*101
+    for n in nums:
+        ans[n] += 1
+    idx = 0
+    num = 0
+    for i in range(100, -1, -1):
+        if num < ans[i]:
+            num = ans[i]
+            idx = i
+    print(f'#{tc} {idx}')
