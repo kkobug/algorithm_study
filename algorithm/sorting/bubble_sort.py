f9ceb03f1d0c7bb39366f6a502b1f6190223dc61
양지훈 <kelvin9149@gmail.com>
2021-10-25 13:08:56 +0900

    Bubble Sort example code

--- algorithm/sorting/bubble_sort.py
+++ algorithm/sorting/bubble_sort.py
@@ -1,12 +1,13 @@
 """
 <오름차순>
 마지막 인덱스 부터 2번째 인덱스 까지 반복
-j번째 인덱스와 j+1번째 읻넥스를 비교하여 크기가 큰 것을 뒤로 이동
+j번째 인덱스와 j+1번째 인덱스를 비교하여 크기가 큰 것을 뒤로 이동
 i 기준 1회 반복 때 마다 가장 큰 값이 맨 뒤로 밀려나므로 j 반복문의 반복길이가 1씩 감소
 """
 
 
-def bubble(num_list):
+arr = [4, 1, 2, 3, 0]
+def bubble_sort(num_list):
     for i in range(len(num_list)-1, 0, -1):
         for j in range(0, i):
             if num_list[j] > num_list[j + 1]:
@@ -15,4 +16,4 @@ def bubble(num_list):
     return num_list
 
 
-print(bubble([4, 3, 2, 1, 0]))
+print(bubble_sort(arr))
