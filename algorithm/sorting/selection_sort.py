5af77534661180610cb587a9b00521c789fddeb0
양지훈 <kelvin9149@gmail.com>
2021-10-25 13:09:27 +0900

    Selection Sort example code

--- algorithm/sorting/selection_sort.py
+++ algorithm/sorting/selection_sort.py
@@ -1,10 +1,11 @@
 """
 첫 번째 인덱스부터 마지막 직전 인덱스까지 반복
-i번째 인덱스와 i+1번째 인덱스 ~ 마지막 인덱스 까지 비교하여 크기가 작은 것을 i번째로 이동
+i번째 인덱스와 (i+1번째 인덱스 ~ 마지막 인덱스 중 가장 작은 값을 갖는 인덱스)를 비교하여 크기가 작은 것을 i번째와 스왑
 """
 
 
-def selection(num_list):
+arr = [4, 1, 2, 3, 0]
+def selection_sort(num_list):
     for i in range(len(num_list)-1):
         temp = i
         for j in range(i+1, len(num_list)):
@@ -15,4 +16,4 @@ def selection(num_list):
     return num_list
 
 
-print(selection([4, 3, 2, 1, 0]))
+print(selection_sort(arr))
