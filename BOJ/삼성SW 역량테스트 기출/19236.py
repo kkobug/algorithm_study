040b29c71b88a4917c9bc99d8e2b4b4f66d63d89
양지훈 <kelvin9149@gmail.com>
2021-11-16 09:39:25 +0900

    BOJ_samsungSWtest update

--- BOJ/삼성SW 역량테스트 기출/19236.py
+++ BOJ/삼성SW 역량테스트 기출/19236.py
@@ -1,103 +1,77 @@
-from pprint import pprint
+"""
+https://www.acmicpc.net/problem/19236
+"""
+from copy import deepcopy
+from sys import stdin
+input = stdin.readline
 
-def play(FOOD, shark, cnt=0):
+di = [-1, -1, 0, 1, 1, 1, 0, -1]
+dj = [0, -1, -1, -1, 0, 1, 1, 1]
+A = [[0]*4 for _ in range(4)]
+F = [[] for _ in range(17)]
+for i in range(4):
+    a1, b1, a2, b2, a3, b3, a4, b4 = map(int, input().split())
+    A[i][0] = a1
+    A[i][1] = a2
+    A[i][2] = a3
+    A[i][3] = a4
+    # 좌표와 방향
+    F[a1] = [i, 0, b1-1]
+    F[a2] = [i, 1, b2-1]
+    F[a3] = [i, 2, b3-1]
+    F[a4] = [i, 3, b4-1]
+
+
+ans = A[0][0]
+S = F[A[0][0]][:]
+F[A[0][0]] = []
+A[0][0] = 0
+
+
+def move(aqua, fish, shark, cnt): #cnt = 먹은 물고기 수
     global ans
-    if not FOOD:
-        return
-
     if ans < cnt:
         ans = cnt
-    move()
-    s_temp = shark[:]
-    for F in FOOD:
-        # 방향, 좌표, 먹은 수
-        shark[0] = aquarium[F[0]][F[1]][1]
-        shark[1], shark[2] = F[0], F[1]
-        shark[3] = aquarium[F[0]][F[1]][0]
-        play(eat(), shark, cnt+shark[3])
-        shark = s_temp[:]
 
-
-def eat():
-    food = []
-    sr, sc = shark[1], shark[2]
-    sd = shark[0]
-    while True:
-        nsr = sr + direction[sd][0]
-        nsc = sc + direction[sd][1]
-        if 0 <= nsr < 4 and 0 <= nsc < 4:
-            if aquarium[nsr][nsc]:
-                food.append((nsr, nsc))
-            sr, sc = nsr, nsc
-        else:
-            break
-    return food
-
-def move():
-    for i in range(1, 17):
-        if not fish[i]:
+    # 물고기 이동!
+    for _fish in fish:
+        if not _fish:
             continue
-        r, c = fish[i]
-        d = aquarium[r][c][1]
-        # f = aquarium[r][c][0]
-        while True:
-            nr = r + direction[d][0]
-            nc = c + direction[d][1]
-            if 0 <= nr < 4 and 0 <= nc < 4 and (nr, nc) != (shark[1], shark[2]):
+        i, j, d = _fish
+        for _ in range(8):
+            ni = i + di[d]
+            nj = j + dj[d]
+            if 0 <= ni < 4 and 0 <= nj < 4 and (ni, nj) != (shark[0], shark[1]):
                 break
             else:
-                d = (d+1) % 8
-        if aquarium[nr][nc]:
-            nf = aquarium[nr][nc][0]
-            # nd = aquarium[nr][nc][1]
-            # 이제 nr과 nc는 i번 물고기(=f)가 갈곳
-            # r, c는 i번 물고기 좌표, nr, nc는 바뀔 좌표
-            # nf랑 nd는 새 물고기랑 새 물고기 방향
-            temp = aquarium[r][c][:]
-            aquarium[r][c] = aquarium[nr][nc][:]
-            aquarium[nr][nc] = temp[:]
+                d = (d+1)%8
+        # ni, nj가 이동할 좌표야!
+        now_fish_num = aqua[i][j]
+        change_fish_num = aqua[ni][nj]
+        aqua[i][j], aqua[ni][nj] = aqua[ni][nj], aqua[i][j]
+        # 물고기 정보를 갱신해!
+        fish[now_fish_num] = [ni, nj, d]
+        # 바뀔 자리에 물고기가 없으면 못바꿔!
+        if fish[change_fish_num]:
+            fish[change_fish_num][0], fish[change_fish_num][1] = i, j
 
-            temp = fish[i][:]
-            fish[i] = fish[nf][:]
-            fish[nf] = temp[:]
-        else:
-            aquarium[nr][nc] = aquarium[r][c][:]
-            aquarium[r][c] = []
-            fish[i] = [nr, nc]
+    si, sj, sd = shark
+    while True:
+        nsi = si + di[sd]
+        nsj = sj + dj[sd]
+        si, sj = nsi, nsj
+        if not (0 <= nsi < 4 and 0 <= nsj < 4):
+            break
+        eat = aqua[nsi][nsj]
+        if eat == 0:
+            continue
+        temp_eat = fish[eat][:]
+        aqua[nsi][nsj] = 0
+        fish[eat] = []
+        move(deepcopy(aqua), deepcopy(fish), temp_eat, cnt+eat)
+        aqua[nsi][nsj] = eat
+        fish[eat] = temp_eat[:]
 
 
-
-direction = [[-1, 0], [-1, -1], [0, -1],
-             [1, -1], [1, 0], [1, 1],
-             [0, 1], [-1, 1]]
-
-# 물고기번호, 방향
-aquarium = [[],
-            [],
-            [],
-            []]
-# i번 물고기 좌표
-fish = [[] for _ in range(17)]
-# 상어 정보(방향, 좌표, 먹은 수)
-shark = [0, 0, 0, 0]
-
-for i in range(4):
-    inp = list(map(int, input().split()))
-    for j in range(4):
-        aquarium[i].append([inp[2*j]] + [inp[2*j+1]-1])
-        fish[inp[2*j]] = [i, j]
-
-# pprint(aquarium)
-# pprint(fish)
-
-shark[0] = aquarium[0][0].pop()
-shark[1], shark[2] = fish[aquarium[0][0][0]]
-shark[3] = aquarium[0][0].pop()
-
-# pprint(shark)
-# pprint(aquarium)
-
-ans = 0
-play(eat(), shark)
-
+move(A, F, S, ans)
 print(ans)
