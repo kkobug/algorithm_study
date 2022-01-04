1c44df6c6437bb1d458a99293d121bd476b38659
양지훈 <kelvin9149@gmail.com>
2022-01-04 09:39:08 +0900

    SWEA update

--- SWEA/DS_Graph/1795.py
+++ SWEA/DS_Graph/1795.py
@@ -1,35 +1,75 @@
-from pprint import pprint
-from collections import deque
+"""
+※ SW Expert 아카데미의 문제를 무단 복제하는 것을 금지합니다.
+인수가 사는 마을에는 N개의 집이 있고, 각 집에는 한 명의 사람이 살고 있다.
+N개의 집을 정점으로 볼 때, 도로는 어떤 집에서 다른 어떤 집으로 이동이 가능한 단방향 간선으로 볼 수 있다.
+도로는 모든 집들 간에 이동이 가능하도록 구성되어 있다.
+집에 1번에서 N번까지의 번호를 붙일 때, 인수의 집은 X번 집이다.
+오늘은 인수의 생일이기 때문에 모든 마을 사람들이 인수의 생일을 축하해주기 위해 X번 집으로 모인다.
+각 사람들은 자신의 집에서 X번 집으로 오고 가기 위해 최단 시간으로 이동한다.
+도로가 단 방향이기 때문에 이용하는 도로는 오고 갈 때 다를 것이다.
 
-for T in range(int(input())):
+[입력]
+첫 번째 줄에 테스트 케이스의 수 T가 주어진다.
+각 테스트 케이스의 첫 번째 줄에는 세 정수 N,M,X(1 ≤ X ≤ N ≤ 1,000, 1 ≤ M ≤ 10,000)가 공백으로 구분되어 주어진다.
+다음 M개의 각 줄에는 세 정수 x, y, c (1 ≤ x, y ＜ N, 1 ≤ c ≤ 100)가 공백으로 구분되어 주어진다.
+이는 x번 집에서 y번 집으로 가는 데 시간이 c가 걸리는 단 방향 도로가 존재한다는 뜻이다.
+
+[출력]
+각 테스트 케이스마다 ‘#x’(x는 테스트케이스 번호를 의미하며 1부터 시작한다)를 출력하고,
+오고 가는데 걸리는 시간이 가장 긴 거리를 출력한다.
+
+입력
+1
+4 8 2
+1 2 4
+1 3 2
+1 4 7
+2 1 1
+2 3 5
+3 1 2
+3 4 4
+4 2 3
+
+출력
+#1 10
+"""
+for tc in range(int(input())):
     N, M, X = map(int, input().split())
-    home = [[] for _ in range(N+1)]
-    dist = [[999999999]*(N+1) for _ in range(N+1)]
+    go_arr = [[] for _ in range(N+1)]
+    back_arr = [[] for _ in range(N+1)]
     for _ in range(M):
         x, y, c = map(int, input().split())
-        dist[x][y] = c
-        home[x].append([y, c])
+        go_arr[x].append([y, c])
+        back_arr[y].append([x, c])
 
-    for i in range(1, N+1):
-        Q = deque([i])
-        dist[i][i] = 0
+    go_dist = [float('inf')] * (N+1)
+    back_dist = [float('inf')] * (N+1)
+    go_dist[X] = back_dist[X] = 0
+    go_visit = [False] * (N+1)
+    back_visit = [False] * (N+1)
 
-        while Q:
-            now = Q.popleft()
+    for _ in range(N):
+        go_st = -1
+        go_val = float('inf')
+        back_st = -1
+        back_val = float('inf')
+        for i in range(1, 1+N):
+            if go_dist[i] < go_val and not go_visit[i]:
+                go_st = i
+                go_val = go_dist[i]
+            if back_dist[i] < back_val and not back_visit[i]:
+                back_st = i
+                back_val = back_dist[i]
 
-            for k in home[now]:
-                ed, c = k
-                d = dist[i][now] + c
-                if d < dist[i][ed]:
-                    dist[i][ed] = d
-                    Q.append(ed)
+        go_visit[go_st] = back_visit[back_st] = True
+        for ed, next_val in go_arr[go_st]:
+            if go_dist[go_st] + next_val < go_dist[ed]:
+                go_dist[ed] = go_dist[go_st] + next_val
+        for ed, next_val in back_arr[back_st]:
+            if back_dist[back_st] + next_val < back_dist[ed]:
+                back_dist[ed] = back_dist[back_st] + next_val
 
-    pprint(dist)
     ans = 0
-    for i in range(N+1):
-        a, b = dist[i][X], dist[X][i]
-        if ans < a and a != 999999999:
-            ans = a
-        if ans < b and b != 999999999:
-            ans = b
-    print(ans)
+    for i in range(1, 1+N):
+        ans = max(ans, go_dist[i]+back_dist[i])
+    print(f'#{tc+1} {ans}')
--- SWEA/DS_Graph/5249.py
+++ SWEA/DS_Graph/5249.py
@@ -1,3 +1,27 @@
+"""
+※ SW Expert 아카데미의 문제를 무단 복제하는 것을 금지합니다.
+그래프에서 사이클을 제거하고 모든 노드를 포함하는 트리를 구성할 때,
+가중치의 합이 최소가 되도록 만든 경우를 최소신장트리라고 한다.
+0번부터 V번까지의 노드와 E개의 간선을 가진 그래프 정보가 주어질 때,
+이 그래프로부터 최소신장트리를 구성하는 간선의 가중치를 모두 더해 출력하는 프로그램을 만드시오.
+
+[입력]
+첫 줄에 테스트 케이스의 개수 T가 주어지고, 테스트 케이스 별로 첫 줄에 마지막 노드번호 V와 간선의 개수 E가 주어진다.
+다음 줄부터 E개의 줄에 걸쳐 간선의 양 끝 노드 n1, n2, 가중치 w가 차례로 주어진다.
+1<=T<=50, 1<=V<=1000, 1<=w<=10, 1<=E<=1000000
+
+[출력]
+각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 답을 출력한다.
+
+입력
+1
+2 3
+0 1 1
+0 2 1
+1 2 6
+출력
+#1 2
+"""
 def find_set(x):
     if P[x] != x:
         P[x] = find_set(P[x])
@@ -17,25 +41,3 @@ for tc in range(int(input())):
         ans += w
         cnt += 1
     print(f'#{tc+1} {ans}')
-
-
-# for tc in range(int(input())):
-#     V, E = map(int, input().split())
-#     tree = [[] for _ in range(V+1)]
-#     visit = [True] + [False]*V
-#     ans = 0
-#     for _ in range(E):
-#         st, ed, w = map(int, input().split())
-#         tree[st].append([ed, w])
-#         tree[ed].append([st, w])
-#
-#     st = 0
-#     while sum(visit) != len(visit):
-#         ed = w = 10
-#         for i in tree[st]:
-#             if i[1] < w and not visit[i[0]]:
-#                 ed, w = i
-#         visit[ed] = True
-#         ans += w
-#         st = ed
-#     print(ans)
