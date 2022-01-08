06959cd7f7fa9904dce1284abe5835e26b703b7c
admin <kelvin9149@gmail.com>
2022-01-08 20:48:54 +0900

    Insert Sort example code

--- algorithm/sorting/insert_sort.py
+++ algorithm/sorting/insert_sort.py
@@ -1,4 +1,11 @@
-def insert(num_list):
+"""
+두 번째 인덱스부터 마지막까지 반복하며
+i번째 인덱스의 앞의 인덱스들을 모두 비교하여 i번째 인덱스의 크기가 더 작아지는 위치에서 삽입
+"""
+
+
+arr = [4, 1, 2, 3, 0]
+def insert_sort(num_list):
     for i in range(1, len(num_list)):
         data = num_list[i]
         j = i
@@ -6,6 +13,6 @@ def insert(num_list):
             num_list[j] = num_list[j-1]
             j -= 1
         num_list[j] = data
-    return num_list # 공간복잡도 N
+    return num_list
 
-print(insert([4, 3, 2 , 1, 0]))
+print(insert_sort(arr))
