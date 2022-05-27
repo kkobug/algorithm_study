f4df5287da88ac79ba4eea86775add8216278b42
admin <kelvin9149@gmail.com>
2022-05-27 15:02:25 +0900

    programmers problems

---
+++ programmers/2xn타일링.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    tile = [1, 2]
+    for i in range(2, n):
+        tile.append((tile[i-1] + tile[i-2]) % 1000000007)
+    answer = tile[-1]
+    return answer
+
+print(solution(600))
